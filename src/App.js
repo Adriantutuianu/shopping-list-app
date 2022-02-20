a456177ea80555fe57d7422b011a23691d2ba09a
Adrian <adriantutuianu01@gmail.com>
2022-02-20 23:53:06 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Shopping list :</header>
+      <Form />
+      <footer>
+        @{new Date().getFullYear()} - All rights reserved @Adrian Tut.
+      </footer>
     </div>
   );
 }
