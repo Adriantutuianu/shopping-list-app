d8471732708fae690705e4a3d69fc47a805d71e0
Adrian <adriantutuianu01@gmail.com>
2022-02-21 23:06:17 +0000

    Added input text - Use State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 import "./App.css";
 import Form from "./components/Form";
 
 function App() {
+  //state
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">Shopping list :</header>
       <div className="content">
-        <Form />
+        <Form inputText={inputText} setInputText={setInputText} />
       </div>
       <footer>
         @{new Date().getFullYear()} - All rights reserved @Adrian Tut.
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Form = () => {
-  return <div>Shopping List</div>;
+const Form = ({ inputText, setInputText }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <form>
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="shopping-input"
+      />
+    </form>
+  );
 };
 
 export default Form;
