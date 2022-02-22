51f67ced75b29ae718a22f181c46afc9bfe7e448
Adrian <adriantutuianu01@gmail.com>
2022-02-22 22:55:38 +0000

    Added event deleteHandler

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-const Shopping = () => {
+const Shopping = ({ text, shop, shopping, setShopping }) => {
+  // Events
+  const deleteHandler = () => {
+    setShopping(shopping.filter((el) => el.id !== shop.id));
+  };
+
   return <div>Shopping</div>;
 };
 
