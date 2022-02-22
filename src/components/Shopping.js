7f65e4238e4d181b4fa22d729710b3d9dd2ad7ed
Adrian <adriantutuianu01@gmail.com>
2022-02-22 22:56:04 +0000

    Added event completeHandler

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -6,6 +6,20 @@ const Shopping = ({ text, shop, shopping, setShopping }) => {
     setShopping(shopping.filter((el) => el.id !== shop.id));
   };
 
+  const completeHandler = () => {
+    setShopping(
+      shopping.map((item) => {
+        if (item.id === shop.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return <div>Shopping</div>;
 };
 
