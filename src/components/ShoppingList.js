3d864768e84a4b0bf4b9ea46b1f2771c32904849
Adrian <adriantutuianu01@gmail.com>
2022-02-22 22:51:00 +0000

    Added props in ShoppingList + return

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Shopping from "./Shopping";
 
-const ShoppingList = () => {
-  return <div>ShoppingList</div>;
+const ShoppingList = ({ shopping, setShopping, filteredShopping }) => {
+  return (
+    <div className="shopping-container">
+      <ul className="shopping-list">
+        {filteredShopping.map((shop) => (
+          <Shopping
+            setShopping={setShopping}
+            shopping={shopping}
+            key={shop.id}
+            shop={shop}
+            text={shop.text}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ShoppingList;
