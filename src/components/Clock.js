65e272d85db995aa420fd159231a4e4da2022361
Adrian <adriantutuianu01@gmail.com>
2022-03-03 23:12:36 +0000

    Live clock -use state

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function Clock() {
-  const date = new Date();
+  const [date, setDate] = useState(new Date());
+
+  function refreshClock() {
+    setDate(new Date());
+  }
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
   return <span>{date.toLocaleTimeString()}</span>;
 }
 export default Clock;
