4623bb0f272d9413a50ac2d343079d770822788b
Adrian <adriantutuianu01@gmail.com>
2022-03-09 23:40:18 +0000

    Styled drawer

--- src/components/Info.js
+++ src/components/Info.js
@@ -30,16 +30,47 @@ const Info = () => {
         onClose={onClose}
         visible={visible}
       >
-        <Title level={5}>Description</Title>
+        <Title
+          style={{
+            color: "var(--secondary-color)",
+            borderTop: " 1px solid var(--secondary-color)",
+            borderBottom: " 1px solid var(--secondary-color)",
+          }}
+          level={5}
+        >
+          Description
+        </Title>
         <Paragraph>
           This is a single page created in React which includes a shopping list.
         </Paragraph>
-        <Title level={5}>Instructions</Title>
+        <Title
+          style={{
+            color: "var(--secondary-color)",
+            borderBottom: " 1px solid var(--secondary-color)",
+          }}
+          level={5}
+        >
+          Instructions
+        </Title>
         <Paragraph>You can add items and quantity in your list.</Paragraph>
         <Paragraph>You can tick them as completed.</Paragraph>
         <Paragraph>You can delete items selected.</Paragraph>
-        <Title level={5}>Credits</Title>
-        <Paragraph>@2022 - All rights reserved @Adrian Tut.</Paragraph>
+        <Title
+          style={{
+            color: "var(--secondary-color)",
+            borderBottom: " 1px solid var(--secondary-color)",
+          }}
+          level={5}
+        >
+          Credits
+        </Title>
+        <Paragraph
+          style={{
+            borderBottom: " 1px solid var(--secondary-color)",
+          }}
+        >
+          @2022 - All rights reserved @Adrian Tut.
+        </Paragraph>
       </Drawer>
     </>
   );
