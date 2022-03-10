1926b5c7d71f755cc71f7a8b812b8e974e8b289e
Adrian <adriantutuianu01@gmail.com>
2022-03-10 00:49:04 +0000

    Prevent submitting if input value = 0

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -21,9 +21,20 @@ const Shopping = ({ text, shop, shopping, setShopping }) => {
     );
   };
 
+  const onChangeQuantity = (e) => {
+    if (e.target.value === "0") {
+      console.log("Is equal to 0. Will not be submitted");
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className="shop">
-      <Input placeholder="Qty" className="input-quantity" />
+      <Input
+        onChange={onChangeQuantity}
+        placeholder="Qty"
+        className="input-quantity"
+      />
       <li className={`shop-item ${shop.completed ? "completed" : ""}`}>
         {text}
       </li>
